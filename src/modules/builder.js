57dadaa65f43a2e9b0af99a0ebb220beadf46d72
Ryan Talbot <ryan-talbot@outlook.com>
2021-09-12 22:51:52 +0100

    building basic home structure via builder

--- src/modules/builder.js
+++ src/modules/builder.js
@@ -1,3 +1,15 @@
+import buildFooter from './footer';
 import buildHeader from './header';
+import buildLanding from './landing';
 
-export default function buildWebsite() {}
+export default function buildWebsite() {
+  const content = document.getElementById('content');
+
+  const container = document.createElement('div');
+  container.classList.add('container');
+  content.appendChild(container);
+
+  container.appendChild(buildHeader());
+  container.appendChild(buildLanding());
+  container.appendChild(buildFooter());
+}
