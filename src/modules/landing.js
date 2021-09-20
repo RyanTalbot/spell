a3e7be20ee9fbbeb70f437053c10fb09e369cced
Ryan Talbot <ryan-talbot@outlook.com>
2021-09-21 00:45:36 +0100

    API response generating icons for current weather

--- src/modules/landing.js
+++ src/modules/landing.js
@@ -3,6 +3,7 @@ import { buildForm } from './search';
 export default function buildLanding() {
   const splash = document.createElement('div');
   splash.classList.add('splash');
+  splash.setAttribute('id', 'splash-area');
 
   splash.appendChild(buildForm());
 
